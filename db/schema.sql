2fc7dc9bb520b6dfe147bfd4e83591392884a4fe
Karin <karin.zaki@hotmail.com>
2021-07-31 15:39:01 +1000

    index and schema

--- db/schema.sql
+++ db/schema.sql
@@ -20,11 +20,9 @@ CREATE TABLE parks(
 ALTER TABLE users
 ADD COLUMN park_id INTEGER;
 
+-- Hard coding lat and lng
+INSERT INTO parks (name, address, lat, lng) VALUES ('Shore Reserve', '223 Reynard St, Pascoe Vale South', '-37.75473954469865', '144.9400667775689');
 
+INSERT INTO parks (name, address, lat, lng) VALUES ('Jacobs Reserve', 'Brunswick West', '-37.756972741', '144.94606131');
 
-Shore Reserve, 223 Reynard St, Pascoe Vale South
-Jacobs Reserve, Brunswick West
-Dunstan Reserve, 24 Peacock St, Brunswick West 
-
-
-
+INSERT INTO parks (name, address, lat, lng) VALUES ('Dunstan Reserve', '24 Peacock St, Brunswick West', '-37.75490494072', '144.9402438');
