1ce9cd13e5390be378bc7ebd53936a1a4b45a168
Rachel <racharon@gmail.com>
2021-08-07 10:34:45 +1000

    sat update

--- db/db.js
+++ db/db.js
@@ -1,10 +1,27 @@
 // visit node.postgres.com for more info
 const pg = require('pg')
+const parse = require('pg-connection-string').parse;
+
+
 
 // not just creating a connection to db, its also creating a pool we can take data out of
-const db = new pg.Pool({
-  database: 'dogwatch',
-  password: process.env.DB_PASSWORD
-})
+// const db = new pg.Pool({
+//   database: 'dogwatch',
+//   password: process.env.DB_PASSWORD
+// })
+
+
+
+let db;
+if (process.env.NODE_ENV === 'production') {
+  const config = parse(process.env.DATABASE_URL)
+  db = new pg.Pool(config)
+  
+} else {
+  db = new pg.Pool({
+    database: 'dogwatch',
+    password: 'optional_password'
+  })
+}
 
 module.exports = db
