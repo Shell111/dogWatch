dff85da5075a21576da2588bc446ea0ced74ec30
Rachel <racharon@gmail.com>
2021-08-09 09:43:54 +1000

    getstarted button fix

--- client/js/get_started.js
+++ client/js/get_started.js
@@ -1,7 +1,7 @@
 function renderGetStarted() {
   document.querySelector('#get-started').innerHTML = `
     <p>Let’s get you started and check out all the parks?</p>
-    <button id="get-started-btn" class="get-started-btn"><a href="/get_started.html">Get Started</a></button>
+    <a href="/get_started.html"><button id="get-started-btn" class="get-started-btn">Get Started</button></a>
     `
 }
 
